8653ffb595828130b5e358cd1902e204f60f036c
Antonio Francisco <ajoaoff@gmail.com>
2020-04-14 13:26:56 -0300

    Make code flatter

--- main.py
+++ main.py
@@ -52,18 +52,15 @@ class Main(KytosNApp):
     @rest('/<mw_id>', methods=['GET'])
     def get_mw(self, mw_id=None):
         """Return one or all maintenance windows."""
-        if mw_id:
-            try:
-                result = self.maintenances[mw_id].as_dict()
-                status = 200
-            except KeyError:
-                result = {'response': f'Maintenance with id {mw_id} not found'}
-                status = 404
-        else:
-            result = [mw.as_dict() for mw in self.maintenances.values()]
-            status = 200
-
-        return jsonify(result), status
+        if mw_id is None:
+            return jsonify(
+                [maintenance.as_dict()
+                 for maintenance in self.maintenances.values()]), 200
+        try:
+            return jsonify(self.maintenances[mw_id].as_dict()), 200
+        except KeyError:
+            return jsonify({'response': f'Maintenance with id {mw_id} not '
+                                        f'found'}), 404
 
     @rest('/', methods=['POST'])
     def create_mw(self):
@@ -71,83 +68,61 @@ class Main(KytosNApp):
         now = datetime.datetime.now(pytz.utc)
         data = request.get_json()
         if not data:
-            result = "Bad request: The request do not have a json."
-            status = 415
-            log.debug('create_mw result %s %s', result, status)
-        else:
-            mw = MW.from_dict(data, self.controller)
-            if mw is None:
-                result = 'One or more items are invalid'
-                status = 400
-            elif mw.start < now:
-                result = 'Start in the past not allowed'
-                status = 400
-            elif mw.end <= mw.start:
-                result = 'End before start not allowed'
-                status = 400
-            else:
-                self.scheduler.add(mw)
-                self.maintenances[mw.id] = mw
-                result = {'mw_id': mw.id}
-                status = 201
-        return jsonify(result), status
+            return jsonify("Bad request: The request do not have a json."), 415
+        maintenance = MW.from_dict(data, self.controller)
+        if maintenance is None:
+            return jsonify('One or more items are invalid'), 400
+        if maintenance.start < now:
+            return jsonify('Start in the past not allowed'), 400
+        if maintenance.end <= maintenance.start:
+            return jsonify('End before start not allowed'), 400
+        self.scheduler.add(maintenance)
+        self.maintenances[maintenance.id] = maintenance
+        return jsonify({'mw_id': maintenance.id}), 201
 
     @rest('/<mw_id>', methods=['PATCH'])
     def update_mw(self, mw_id):
         """Update a maintenance window."""
         data = request.get_json()
         if not data:
-            result = "Bad request: The request do not have a json."
-            status = 415
-            log.debug('update_mw result %s %s', result, status)
-        else:
-            try:
-                mw = self.maintenances[mw_id]
-                mw.update(data, self.controller)
-                result = {'response': f'Maintenance {mw_id} updated'}
-                status = 201
-            except KeyError:
-                result = {'response': f'Maintenance with id {mw_id} not found'}
-                status = 404
-        return jsonify(result), status
+            return jsonify("Bad request: The request do not have a json."), 415
+        try:
+            maintenance = self.maintenances[mw_id]
+        except KeyError:
+            return jsonify({'response': f'Maintenance with id {mw_id} not '
+                                        f'found'}), 404
+        maintenance.update(data, self.controller)
+        return jsonify({'response': f'Maintenance {mw_id} updated'}), 201
 
     @rest('/<mw_id>', methods=['DELETE'])
     def remove_mw(self, mw_id):
         """Delete a maintenance window."""
         try:
-            mw = self.maintenances[mw_id]
-            self.scheduler.remove(mw)
-            del self.maintenances[mw_id]
-            result = {'response': f'Maintenance with id {mw_id} successfully '
-                                  f'removed'}
-            status = 200
+            maintenance = self.maintenances[mw_id]
         except KeyError:
-            result = {'response': f'Maintenance with id {mw_id} not found'}
-            status = 404
-        return jsonify(result), status
+            return jsonify({'response': f'Maintenance with id {mw_id} not '
+                                        f'found'}), 404
+        self.scheduler.remove(maintenance)
+        del self.maintenances[mw_id]
+        return jsonify({'response': f'Maintenance with id {mw_id} '
+                                    f'successfully removed'}), 200
 
     @rest('/<mw_id>/end', methods=['PATCH'])
     def end_mw(self, mw_id):
         """Finish a maintenance window right now."""
         try:
-            mw = self.maintenances[mw_id]
+            maintenance = self.maintenances[mw_id]
         except KeyError:
-            result = {'response': f'Maintenance with id {mw_id} not found'}
-            status = 404
-        else:
-            now = datetime.datetime.now(pytz.utc)
-            log.info(f'Agora {now}')
-            if now < mw.start:
-                result = {'response': f'Maintenance window {mw_id} has not '
-                                      f'yet started.'}
-                status = 400
-            elif now > mw.end:
-                result = {'response': f'Maintenance window {mw_id} has '
-                                      f'already finished.'}
-                status = 400
-            else:
-                self.scheduler.remove(mw)
-                mw.end_mw()
-                result = {'response': f'Maintenance window {mw_id} finished.'}
-                status = 200
-        return jsonify(result), status
+            return jsonify({'response': f'Maintenance with id '
+                                        f'{mw_id} not found'}), 404
+        now = datetime.datetime.now(pytz.utc)
+        if now < maintenance.start:
+            return jsonify({'response': f'Maintenance window {mw_id} has not '
+                                        f'yet started.'}), 400
+        if now > maintenance.end:
+            return jsonify({'response': f'Maintenance window {mw_id} has '
+                                        f'already finished.'}), 400
+        self.scheduler.remove(maintenance)
+        maintenance.end_mw()
+        return jsonify({'response': f'Maintenance window {mw_id} '
+                                    f'finished.'}), 200
