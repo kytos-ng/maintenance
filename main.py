4173b0a597c360077019dbd68f45b8a5487d5948
David Ramirez <davramir@fiu.edu>
2022-11-04 11:38:42 -0400

    Added persistence to maintenance windows:  - Maintenance Windows now stored to MongoDB  - Events now use IDs rather than instance of objects  - Added cattrs as a dependency for parsing requests  - Added support for links and interfaces

--- main.py
+++ main.py
@@ -3,15 +3,32 @@
 This NApp creates maintenance windows, allowing the maintenance of network
 devices (switch, link, and interface) without receiving alerts.
 """
-import datetime
+from datetime import datetime, timedelta
 
 import pytz
 from flask import jsonify, request
+from cattrs import BaseValidationError, Converter
+from attrs import evolve
 from napps.kytos.maintenance.models import MaintenanceWindow as MW
-from napps.kytos.maintenance.models import Scheduler, Status
+from napps.kytos.maintenance.models import Scheduler, Status, MaintenanceID
 from werkzeug.exceptions import BadRequest, NotFound, UnsupportedMediaType
 
 from kytos.core import KytosNApp, rest
+
+TIME_FMT = "%Y-%m-%dT%H:%M:%S%z"
+
+def time_encoder(ts, cls):
+    if isinstance(ts, datetime):
+        return ts
+    if isinstance(ts, str):
+        return cls.strptime(ts, TIME_FMT)
+
+def time_decoder(ts: datetime):
+    return ts.strftime(TIME_FMT)
+
+converter = Converter()
+converter.register_structure_hook(datetime, time_encoder)
+converter.register_unstructure_hook(datetime, time_decoder)
 
 
 class Main(KytosNApp):
@@ -28,8 +45,8 @@ class Main(KytosNApp):
 
         So, if you have any setup routine, insert it here.
         """
-        self.maintenances = {}
-        self.scheduler = Scheduler()
+        self.scheduler = Scheduler.new_scheduler(self.controller)
+        self.scheduler.start()
 
     def execute(self):
         """Run after the setup method execution.
@@ -45,31 +62,32 @@ class Main(KytosNApp):
 
         If you have some cleanup procedure, insert it here.
         """
+        self.scheduler.shutdown()
 
     @rest('/v1', methods=['GET'])
+    def get_all_mw(self):
+        """Return all maintenance windows."""
+        return jsonify(converter.unstructure(self.scheduler.listMaintenances())), 200
+
     @rest('/v1/<mw_id>', methods=['GET'])
-    def get_mw(self, mw_id=None):
-        """Return one or all maintenance windows."""
-        if mw_id is None:
-            return jsonify(
-                [maintenance.as_dict()
-                 for maintenance in self.maintenances.copy().values()]), 200
+    def get_mw(self, mw_id: MaintenanceID):
+        """Return one maintenance window."""
         try:
-            return jsonify(self.maintenances[mw_id].as_dict()), 200
+            return jsonify(converter.unstructure(self.scheduler.getMaintenance(mw_id))), 200
         except KeyError:
             raise NotFound(f'Maintenance with id {mw_id} not found')
 
     @rest('/v1', methods=['POST'])
     def create_mw(self):
         """Create a new maintenance window."""
-        now = datetime.datetime.now(pytz.utc)
+        now = datetime.now(pytz.utc)
         data = request.get_json()
         if not data:
             raise UnsupportedMediaType('The request does not have a json.')
         try:
-            maintenance = MW.from_dict(data, self.controller)
-        except ValueError as err:
-            raise BadRequest(f'{err}')
+            maintenance = converter.structure(data, MW)
+        except BaseValidationError as err:
+            raise BadRequest(f'{err.exceptions}')
         if maintenance is None:
             raise BadRequest('One or more items are invalid')
         if maintenance.start < now:
@@ -77,59 +95,55 @@ class Main(KytosNApp):
         if maintenance.end <= maintenance.start:
             raise BadRequest('End before start not allowed')
         self.scheduler.add(maintenance)
-        self.maintenances[maintenance.id] = maintenance
         return jsonify({'mw_id': maintenance.id}), 201
 
     @rest('/v1/<mw_id>', methods=['PATCH'])
-    def update_mw(self, mw_id):
+    def update_mw(self, mw_id: MaintenanceID):
         """Update a maintenance window."""
         data = request.get_json()
         if not data:
             raise UnsupportedMediaType('The request does not have a json.')
-        try:
-            maintenance = self.maintenances[mw_id]
-        except KeyError:
+        old_maintenance = self.scheduler.getMaintenance(mw_id)
+        if old_maintenance is None:
             raise NotFound(f'Maintenance with id {mw_id} not found')
-        if maintenance.status == Status.RUNNING:
+        if old_maintenance.status == Status.RUNNING:
             raise BadRequest('Updating a running maintenance is not allowed')
         try:
-            maintenance.update(data)
-        except ValueError as error:
-            raise BadRequest(f'{error}')
-        self.scheduler.remove(maintenance)
-        self.scheduler.add(maintenance)
+            new_maintenance = converter.structure({**converter.unstructure(old_maintenance), **data}, MW)
+        except BaseValidationError as err:
+            raise BadRequest(f'{err}')
+        self.scheduler.remove(mw_id)
+        self.scheduler.add(new_maintenance)
         return jsonify({'response': f'Maintenance {mw_id} updated'}), 200
 
     @rest('/v1/<mw_id>', methods=['DELETE'])
-    def remove_mw(self, mw_id):
+    def remove_mw(self, mw_id: MaintenanceID):
         """Delete a maintenance window."""
-        try:
-            maintenance = self.maintenances[mw_id]
-        except KeyError:
+        maintenance = self.scheduler.getMaintenances(mw_id)
+        if maintenance is None:
             raise NotFound(f'Maintenance with id {mw_id} not found')
         if maintenance.status == Status.RUNNING:
             raise BadRequest('Deleting a running maintenance is not allowed')
-        self.scheduler.remove(maintenance)
-        del self.maintenances[mw_id]
+        self.scheduler.remove(mw_id)
         return jsonify({'response': f'Maintenance with id {mw_id} '
                                     f'successfully removed'}), 200
 
     @rest('/v1/<mw_id>/end', methods=['PATCH'])
-    def end_mw(self, mw_id):
+    def end_mw(self, mw_id: MaintenanceID):
         """Finish a maintenance window right now."""
-        try:
-            maintenance = self.maintenances[mw_id]
-        except KeyError:
+        maintenance = self.scheduler.getMaintenances(mw_id)
+        if maintenance is None:
             raise NotFound(f'Maintenance with id {mw_id} not found')
-        now = datetime.datetime.now(pytz.utc)
-        if now < maintenance.start:
-            raise BadRequest(f'Maintenance window {mw_id} has not yet '
-                             'started.')
-        if now > maintenance.end:
-            raise BadRequest(f'Maintenance window {mw_id} has already '
-                             'finished.')
-        self.scheduler.remove(maintenance)
-        maintenance.end_mw()
+        match maintenance:
+            case MW(status = Status.PENDING):
+                raise BadRequest(
+                    f'Maintenance window {mw_id} has not yet started'
+                )
+            case MW(status = Status.FINISHED):
+                raise BadRequest(
+                    f'Maintenance window {mw_id} has already finished'
+                )
+        self.scheduler.endMaintenanceEarly(mw_id)
         return jsonify({'response': f'Maintenance window {mw_id} '
                                     f'finished.'}), 200
 
@@ -139,24 +153,28 @@ class Main(KytosNApp):
         data = request.get_json()
         if not data:
             raise UnsupportedMediaType('The request does not have a json')
-        try:
-            maintenance = self.maintenances[mw_id]
-        except KeyError:
+        maintenance = self.scheduler.getMaintenance(mw_id)
+        if maintenance is None:
             raise NotFound(f'Maintenance with id {mw_id} not found')
         if 'minutes' not in data:
             raise BadRequest('Minutes of extension must be sent')
-        now = datetime.datetime.now(pytz.utc)
-        if now < maintenance.start:
-            raise BadRequest(f'Maintenance window {mw_id} has not yet '
-                             'started')
-        if now > maintenance.end:
-            raise BadRequest(f'Maintenance window {mw_id} has already '
-                             'finished')
+        now = datetime.now(pytz.utc)
+        match maintenance:
+            case MW(status = Status.PENDING):
+                raise BadRequest(
+                    f'Maintenance window {mw_id} has not yet started'
+                )
+            case MW(status = Status.FINISHED):
+                raise BadRequest(
+                    f'Maintenance window {mw_id} has already finished'
+                )
         try:
-            maintenance.end = maintenance.end + \
-                datetime.timedelta(minutes=data['minutes'])
+            maintenance_end = maintenance.end + \
+                timedelta(minutes=data['minutes'])
+            new_maintenance = evolve(maintenance, end = maintenance_end)
         except TypeError:
             raise BadRequest('Minutes of extension must be integer')
+        
         self.scheduler.remove(maintenance)
-        self.scheduler.add(maintenance)
+        self.scheduler.add(new_maintenance)
         return jsonify({'response': f'Maintenance {mw_id} extended'}), 200
--- models.py
+++ models.py
@@ -3,228 +3,245 @@
 This module define models for the maintenance window itself and the
 scheduler.
 """
-import datetime
-from enum import IntEnum
+from datetime import datetime
+from enum import Enum
+from typing import NewType
 from uuid import uuid4
+from kytos.core.db import Mongo
 
 import pytz
+from attrs import define, field
+from cattrs import Converter
+
 from apscheduler.jobstores.base import JobLookupError
+from apscheduler.schedulers.base import BaseScheduler
 from apscheduler.schedulers.background import BackgroundScheduler
 
+from pymongo.collection import Collection
+
 from kytos.core import KytosEvent, log
-from kytos.core.interface import TAG, UNI
-from kytos.core.link import Link
+from kytos.core.controller import Controller
 
 TIME_FMT = "%Y-%m-%dT%H:%M:%S%z"
 
+def time_encoder(ts, cls):
+    if isinstance(ts, datetime):
+        return ts
+    if isinstance(ts, str):
+        return cls.strptime(ts, TIME_FMT)
 
-class Status(IntEnum):
+converter = Converter()
+converter.register_structure_hook(datetime, lambda ts, cl: ts)
+
+class Status(str, Enum):
     """Maintenance windows status."""
 
-    PENDING = 0
-    RUNNING = 1
-    FINISHED = 2
+    PENDING = 'pending'
+    RUNNING = 'running'
+    FINISHED = 'finished'
 
+MaintenanceID = NewType('MaintenanceID', str)
 
+@define(frozen=True)
 class MaintenanceWindow:
-    """Class to store a maintenance window."""
+    start: datetime
+    end: datetime
+    switches: list[str] = field(factory = list)
+    interfaces: list[str] = field(factory = list)
+    links: list[str] = field(factory = list)
+    id: MaintenanceID = field(factory = lambda:MaintenanceID(uuid4().hex))
+    description: str | None = field(default=None)
+    status: Status = field(default=Status.PENDING)
 
-    def __init__(self, start, end, controller, **kwargs):
-        """Create an instance of MaintenanceWindow.
-
-        Args:
-            start(datetime): when the maintenance will begin
-            end(datetime): when the maintenance will finish
-            items: list of items that will be maintained;
-                each item can be either a switch, a link or a client interface
-        """
-        # pylint: disable=invalid-name
-        self.controller = controller
-        items = kwargs.get('items')
-        if items is None:
-            items = []
-        mw_id = kwargs.get('mw_id')
-        self.id = mw_id if mw_id else uuid4().hex
-        self.description = kwargs.get('description')
-        self.start = start
-        self.end = end
-        self._switches = []
-        self._links = []
-        self._unis = []
-        self.items = items
-        self.status = kwargs.get('status', Status.PENDING)
-
-    @property
-    def items(self):
-        """Items getter."""
-        return self._switches + self._links + self._unis
-
-    @items.setter
-    def items(self, items):
-        """Items setter."""
-        self._switches = []
-        self._unis = []
-        self._links = []
-        for i in items:
-            if isinstance(i, UNI):
-                self._unis.append(i)
-            elif isinstance(i, Link):
-                self._links.append(i)
-            else:
-                self._switches.append(i)
-
-    def as_dict(self):
-        """Return this maintenance window as a dictionary."""
-        mw_dict = {}
-        mw_dict['id'] = self.id
-        mw_dict['description'] = self.description if self.description else ''
-        mw_dict['status'] = self.status
-        mw_dict['start'] = self.start.strftime(TIME_FMT)
-        mw_dict['end'] = self.end.strftime(TIME_FMT)
-        mw_dict['items'] = []
-        for i in self.items:
-            try:
-                mw_dict['items'].append(i.as_dict())
-            except (AttributeError, TypeError):
-                mw_dict['items'].append(i)
-        return mw_dict
-
-    @classmethod
-    def from_dict(cls, mw_dict, controller):
-        """Create a maintenance window from a dictionary of attributes."""
-        mw_id = mw_dict.get('id')
-
-        start = cls.str_to_datetime(mw_dict['start'])
-        end = cls.str_to_datetime(mw_dict['end'])
-        try:
-            items = mw_dict['items']
-        except KeyError:
-            raise ValueError('At least one item must be provided')
-        if not items:
-            raise ValueError('At least one item must be provided')
-        description = mw_dict.get('description')
-        status = mw_dict.get('status', Status.PENDING)
-        return cls(start, end, controller, items=items, mw_id=mw_id,
-                   description=description, status=status)
-
-    def update(self, mw_dict):
-        """Update a maintenance window with the data from a dictionary."""
-        try:
-            start = self.str_to_datetime(mw_dict['start'])
-        except KeyError:
-            start = self.start
-        try:
-            end = self.str_to_datetime(mw_dict['end'])
-        except KeyError:
-            end = self.end
-        now = datetime.datetime.now(pytz.utc)
-        if start < now:
-            raise ValueError('Start in the past not allowed.')
-        if end < start:
-            raise ValueError('End before start not allowed.')
-        if 'items' in mw_dict:
-            if not mw_dict['items']:
-                raise ValueError('At least one item must be provided')
-            self.items = mw_dict['items']
-        self.start = start
-        self.end = end
-        if 'description' in mw_dict:
-            self.description = mw_dict['description']
-
-    @staticmethod
-    def intf_from_dict(intf_id, controller):
-        """Get the Interface instance with intf_id."""
-        intf = controller.get_interface_by_id(intf_id)
-        return intf
-
-    @staticmethod
-    def uni_from_dict(uni_dict, controller):
-        """Create UNI instance from a dictionary."""
-        intf = MaintenanceWindow.intf_from_dict(uni_dict['interface_id'],
-                                                controller)
-        tag = TAG.from_dict(uni_dict['tag'])
-        if intf and tag:
-            return UNI(intf, tag)
-        return None
-
-    @staticmethod
-    def link_from_dict(link_dict, controller):
-        """Create a link instance from a dictionary."""
-        endpoint_a = controller.get_interface_by_id(
-            link_dict['endpoint_a']['id'])
-        endpoint_b = controller.get_interface_by_id(
-            link_dict['endpoint_b']['id'])
-
-        link = Link(endpoint_a, endpoint_b)
-        if 'metadata' in link_dict:
-            link.extend_metadata(link_dict['metadata'])
-        s_vlan = link.get_metadata('s_vlan')
-        if s_vlan:
-            tag = TAG.from_dict(s_vlan)
-            link.update_metadata('s_vlan', tag)
-        return link
-
-    @staticmethod
-    def str_to_datetime(str_date):
-        """Convert a string representing a date and time to datetime."""
-        date = datetime.datetime.strptime(str_date, TIME_FMT)
-        return date.astimezone(pytz.utc)
-
-    def maintenance_event(self, operation):
+    def maintenance_event(self, operation, controller: Controller):
         """Create events to start/end a maintenance."""
-        if self._switches:
-            switches = []
-            for dpid in self._switches:
-                switch = self.controller.switches.get(dpid, None)
-                if switch:
-                    switches.append(switch)
-            event = KytosEvent(name=f'kytos/maintenance.{operation}_switch',
-                               content={'switches': switches})
-            self.controller.buffers.app.put(event)
-        if self._unis:
-            event = KytosEvent(name=f'kytos/maintenance.{operation}_uni',
-                               content={'unis': self._unis})
-            self.controller.buffers.app.put(event)
-        if self._links:
-            event = KytosEvent(name=f'kytos/maintenance.{operation}_link',
-                               content={'links': self._links})
-            self.controller.buffers.app.put(event)
+        if self.switches:
+            event = KytosEvent(
+                name=f'kytos/maintenance.{operation}_switch',
+                content={'switches': self.switches}
+            )
+            controller.buffers.app.put(event)
+        if self.interfaces:
+            event = KytosEvent(
+                name=f'kytos/maintenance.{operation}_interface',
+                content={'unis': self.interfaces}
+            )
+            controller.buffers.app.put(event)
+        if self.links:
+            event = KytosEvent(
+                name=f'kytos/maintenance.{operation}_link',
+                content={'links': self.links}
+            )
+            controller.buffers.app.put(event)
 
-    def start_mw(self):
+    def start_mw(self, controller: Controller):
         """Actions taken when a maintenance window starts."""
         self.status = Status.RUNNING
-        self.maintenance_event('start')
+        self.maintenance_event('start', controller)
 
-    def end_mw(self):
+    def end_mw(self, controller: Controller):
         """Actions taken when a maintenance window finishes."""
         self.status = Status.FINISHED
-        self.maintenance_event('end')
+        self.maintenance_event('end', controller)
+
+@define
+class MaintenanceStart:
+    maintenance_scheduler: 'Scheduler'
+    id: MaintenanceID
+
+    def __call__(self):
+        self.maintenance_scheduler.startMaintenance(self.id)
 
 
+@define
+class MaintenanceEnd:
+    maintenance_scheduler: 'Scheduler'
+    id: MaintenanceID
+
+    def __call__(self):
+        self.maintenance_scheduler.endMaintenance(self.id)
+
+
+
+@define
 class Scheduler:
     """Scheduler for a maintenance window."""
+    controller: Controller
+    windows: Collection
+    scheduler: BaseScheduler
+    #windows: dict[MaintenanceID, MaintenanceWindow] = field(factory=list)
 
-    def __init__(self):
-        """Initialize a new scheduler."""
-        self.scheduler = BackgroundScheduler(timezone=pytz.utc)
+    @classmethod
+    def new_scheduler(cls, controller: Controller):
+        db = Mongo.client[Mongo.db_name]
+        scheduler = BackgroundScheduler(timezone=pytz.utc)
+        windows = db['maintenance']['windows']
+        windows.create_index('id')
+        instance = cls(controller, windows, scheduler)
+        return instance
+
+    def start(self):
+        # Populate the scheduler with all pending tasks
+        windows = self._db_list_windows()
+        for window in windows:
+            self._schedule(window)
+
+        # Start the scheduler
         self.scheduler.start()
 
-    def add(self, maintenance):
-        """Add jobs to start and end a maintenance window."""
-        self.scheduler.add_job(maintenance.start_mw, 'date',
-                               id=f'{maintenance.id}-start',
-                               run_date=maintenance.start)
-        self.scheduler.add_job(maintenance.end_mw, 'date',
-                               id=f'{maintenance.id}-end',
-                               run_date=maintenance.end)
+    def shutdown(self):
+        self.scheduler.remove_all_jobs()
+        self.scheduler.shutdown()
+        windows = self._db_list_windows()
 
-    def remove(self, maintenance):
+        # Depopulate the scheduler
+        for window in windows:
+            self._unschedule(window)
+        
+
+    def startMaintenance(self, id: MaintenanceID):
+        # Get Maintenance from DB
+        window = self._db_get_window(id)
+
+        # Set to Running
+        window.start_mw(self.controller)
+
+        # Update DB
+        self._db_udate_window(window)
+
+    def endMaintenance(self, id: MaintenanceID):
+        # Get Maintenance from DB
+        window = self._db_get_window(id)
+
+        # Set to Ending
+        window.end_mw(self.controller)
+
+        # Update DB
+        self._db_udate_window(window)
+
+    def endMaintenanceEarly(self, id: MaintenanceID):
+        # Get Maintenance from DB
+        window = self._db_get_window(id)
+
+        # Set to Ending
+        self._unschedule(window)
+
+        # Update DB
+        self._db_udate_window(window)
+
+    def add(self, window:MaintenanceWindow):
+        """Add jobs to start and end a maintenance window."""
+
+        # Add window to DB
+        self.windows.insert_one(converter.unstructure(window))
+
+        # Schedule window
+        self._schedule(window)
+
+
+    def remove(self, id: MaintenanceID):
         """Remove jobs that start and end a maintenance window."""
+        # Get Maintenance from DB
+        window = self._db_get_window(id)
+
+        # Remove from schedule
+        self._unschedule(window)
+
+        # Remove from DB
+        self.windows.delete_one({'id': id})
+
+    def _db_get_window(self, win_id: MaintenanceID):
+        window = self.windows.find_one({'id': win_id}, projection={'_id':False})
+        if window is None:
+            return
+        window: MaintenanceWindow = converter.structure(window, MaintenanceWindow)
+        return window
+
+    def _db_udate_window(self, window: MaintenanceWindow):
+        self.windows.update_one({'id': window.id}, window)
+
+    def _db_list_windows(self) -> list[MaintenanceWindow]:
+        windows = self.windows.find(projection={'_id':False})
+        return list(map(lambda win: converter.structure(win, MaintenanceWindow), windows))
+
+    def _schedule(self, window: MaintenanceWindow):
+        match window:
+            case MaintenanceWindow(status=Status.PENDING):
+                self.scheduler.add_job(MaintenanceStart(self, window.id), 'date',
+                                id=f'{window.id}-start',
+                                run_date=window.start)
+                self.scheduler.add_job(MaintenanceEnd(self, window.id), 'date',
+                                    id=f'{window.id}-end',
+                                    run_date=window.end)
+            case MaintenanceWindow(status=Status.RUNNING):
+                window.start_mw(self.controller)
+                self.scheduler.add_job(MaintenanceEnd(self, window.id), 'date',
+                                    id=f'{window.id}-end',
+                                    run_date=window.end)
+
+    def _unschedule(self, window:MaintenanceWindow):
+        """Remove maintenance events from scheduler.
+        Does not update DB, due to being
+        primarily for shutdown startup cases.
+        """
+        started = False
+        ended = False
         try:
-            self.scheduler.remove_job(f'{maintenance.id}-start')
+            self.scheduler.remove_job(f'{window.id}-start')
         except JobLookupError:
-            log.info(f'Job to start {maintenance.id} already removed.')
+            started = True
+            log.info(f'Job to start {window.id} already removed.')
         try:
-            self.scheduler.remove_job(f'{maintenance.id}-end')
+            self.scheduler.remove_job(f'{window.id}-end')
         except JobLookupError:
-            log.info(f'Job to end {maintenance.id} already removed.')
+            ended = True
+            log.info(f'Job to end {window.id} already removed.')
+        if started and not ended:
+            window.end_mw(self.controller)
+
+
+    def getMaintenance(self, id: MaintenanceID):
+        return self._db_get_window(id)
+
+    def listMaintenances(self):
+        return self._db_list_windows()
