d63cf197a13f96ab0eb6c943d0d6ea4a230a865a
David Ramirez <davramir@fiu.edu>
2022-12-08 10:04:19 -0500

    Moved db operations into controllers submodule

---
+++ controllers/__init__.py
@@ -0,0 +1,77 @@
+"""MaintenanceController."""
+
+# pylint: disable=invalid-name
+import os
+
+from bson.codec_options import CodecOptions
+import pymongo
+from pymongo.errors import AutoReconnect
+from tenacity import retry_if_exception_type, stop_after_attempt, wait_random
+
+from kytos.core import log
+from kytos.core.db import Mongo
+from kytos.core.retry import before_sleep, for_all_methods, retries
+from napps.kytos.maintenance.models import MaintenanceWindow, MaintenanceID
+
+
+@for_all_methods(
+    retries,
+    stop=stop_after_attempt(
+        int(os.environ.get("MONGO_AUTO_RETRY_STOP_AFTER_ATTEMPT", 3))
+    ),
+    wait=wait_random(
+        min=int(os.environ.get("MONGO_AUTO_RETRY_WAIT_RANDOM_MIN", 0.1)),
+        max=int(os.environ.get("MONGO_AUTO_RETRY_WAIT_RANDOM_MAX", 1)),
+    ),
+    before_sleep=before_sleep,
+    retry=retry_if_exception_type((AutoReconnect,)),
+)
+class MaintenanceController:
+    """MaintenanceController."""
+
+    def __init__(self, get_mongo=lambda: Mongo()) -> None:
+        """Constructor of MaintenanceController."""
+        self.mongo = get_mongo()
+        self.db_client = self.mongo.client
+        self.db = self.db_client[self.mongo.db_name]
+        self.windows = self.db['maintenance.windows'].with_options(
+            codec_options=CodecOptions(
+                tz_aware=True,
+            )
+        )
+
+    def bootstrap_indexes(self) -> None:
+        """Bootstrap all maintenance related indexes."""
+        index_tuples = [
+            ("maintenance.windows", [("id", pymongo.ASCENDING)]),
+        ]
+        for collection, keys in index_tuples:
+            if self.mongo.bootstrap_index(collection, keys):
+                log.info(
+                    f"Created DB index {keys}, collection: {collection})"
+                )
+
+    def add_window(self, window: MaintenanceWindow):
+        self.windows.insert_one(window.dict())
+
+    def get_window(self, mw_id: MaintenanceID) -> MaintenanceWindow:
+        window = self.windows.find_one(
+            {'id': mw_id},
+            projection = {'_id': False}
+        )
+        if window is not None:
+            window = MaintenanceWindow.construct(**window)
+        return window
+
+    def udpate_window(self, window: MaintenanceWindow):
+        self.windows.update_one(
+            {'id': window.id},
+            {'$set': window.dict()}
+        )
+
+    def get_windows(self) -> list[MaintenanceWindow]:
+        windows = self.windows.find(projection={'_id': False})
+        return list(windows)
+
+    def remove_window(self, mw_id: MaintenanceID):
+        self.windows.delete_one({'id': mw_id})
--- models.py
+++ models.py
@@ -13,10 +13,7 @@ import pytz
 from apscheduler.jobstores.base import JobLookupError
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.schedulers.base import BaseScheduler
-from bson.codec_options import CodecOptions
 from pydantic import BaseModel, Field
-from pymongo import MongoClient
-from pymongo.collection import Collection
 
 from kytos.core import KytosEvent, log
 from kytos.core.controller import Controller
@@ -107,8 +104,7 @@ class MaintenanceEnd:
 class Scheduler:
     """Scheduler for a maintenance window."""
     controller: Controller
-    db_client: MongoClient
-    windows: Collection
+    db: 'MaintenanceController'
     scheduler: BaseScheduler
 
     @classmethod
@@ -116,16 +112,11 @@ class Scheduler:
         """
         Creates a new scheduler from the given kytos controller
         """
-        db_client = controller.db_client
-        database = controller.db
         scheduler = BackgroundScheduler(timezone=pytz.utc)
-        windows = database['maintenance.windows'].with_options(
-            codec_options=CodecOptions(
-                tz_aware=True,
-            )
-        )
-        windows.create_index('id')
-        instance = cls(controller, db_client, windows, scheduler)
+        from napps.kytos.maintenance.controllers import MaintenanceController
+        db = MaintenanceController()
+        db.bootstrap_indexes()
+        instance = cls(controller, db, scheduler)
         return instance
 
     def start(self):
@@ -133,7 +124,7 @@ class Scheduler:
         Begin running the scheduler.
         """
         # Populate the scheduler with all pending tasks
-        windows = self._db_list_windows()
+        windows = self.db.get_windows()
         for window in windows:
             self._schedule(window)
 
@@ -146,7 +137,7 @@ class Scheduler:
         """
         self.scheduler.remove_all_jobs()
         self.scheduler.shutdown()
-        windows = self._db_list_windows()
+        windows = self.db.get_windows()
 
         # Depopulate the scheduler
         for window in windows:
@@ -156,43 +147,43 @@ class Scheduler:
         """Begins executing the maintenance window
         """
         # Get Maintenance from DB
-        window = self._db_get_window(mw_id)
+        window = self.db.get_window(mw_id)
 
         # Set to Running
         next_win = window.start_mw(self.controller)
 
         # Update DB
-        self._db_update_window(next_win)
+        self.db.update_window(next_win)
 
     def end_maintenance(self, mw_id: MaintenanceID):
         """Ends execution of the maintenance window
         """
         # Get Maintenance from DB
-        window = self._db_get_window(mw_id)
+        window = self.db.get_window(mw_id)
 
         # Set to Ending
         next_win = window.end_mw(self.controller)
 
         # Update DB
-        self._db_update_window(next_win)
+        self.db.update_window(next_win)
 
     def end_maintenance_early(self, mw_id: MaintenanceID):
         """Ends execution of the maintenance window early
         """
         # Get Maintenance from DB
-        window = self._db_get_window(mw_id)
+        window = self.db.get_window(mw_id)
 
         # Set to Ending
         next_win = self._unschedule(window)
 
         # Update DB
-        self._db_update_window(next_win)
+        self.db.update_window(next_win)
 
     def add(self, window: MaintenanceWindow):
         """Add jobs to start and end a maintenance window."""
 
         # Add window to DB
-        self.windows.insert_one(window.dict())
+        self.db.add_window(window)
 
         # Schedule window
         self._schedule(window)
@@ -200,32 +191,13 @@ class Scheduler:
     def remove(self, mw_id: MaintenanceID):
         """Remove jobs that start and end a maintenance window."""
         # Get Maintenance from DB
-        window = self._db_get_window(mw_id)
+        window = self.db.get_window(mw_id)
 
         # Remove from schedule
         self._unschedule(window)
 
         # Remove from DB
-        self.windows.delete_one({'id': mw_id})
-
-    def _db_get_window(self, mw_id: MaintenanceID):
-        window = self.windows.find_one(
-            {'id': mw_id},
-            projection = {'_id': False}
-        )
-        if window is not None:
-            window = MaintenanceWindow.construct(**window)
-        return window
-
-    def _db_update_window(self, window: MaintenanceWindow):
-        self.windows.update_one(
-            {'id': window.id},
-            {'$set': window.dict()}
-        )
-
-    def _db_list_windows(self) -> list[MaintenanceWindow]:
-        windows = self.windows.find(projection={'_id': False})
-        return list(windows)
+        self.db.remove_window(mw_id)
 
     def _schedule(self, window: MaintenanceWindow):
         if window.status is Status.PENDING:
@@ -273,8 +245,8 @@ class Scheduler:
 
     def get_maintenance(self, mw_id: MaintenanceID):
         """Get a single maintenance by id"""
-        return self._db_get_window(mw_id)
+        return self.db.get_window(mw_id)
 
     def list_maintenances(self):
         """Returns a list of all maintenances"""
-        return self._db_list_windows()
+        return self.db.get_windows()
