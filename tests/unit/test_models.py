01399092bd25512b7193bcb06dcbb755495d1ea3
David Ramirez <davramir@fiu.edu>
2023-06-12 22:03:11 +0000

    Updated MaintenanceDeployer unit tests

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -95,9 +95,112 @@ class TestDeployer(TestCase):
         )
 
         self.deployer = MaintenanceDeployer(self.controller, Counter(), Counter(), Counter())
+        # Initialize Switches
+        self.controller.switches = {
+            '01:23:45:67:89:ab:cd:ef': MagicMock(
+                id='01:23:45:67:89:ab:cd:ef',
+                interfaces = {},
+            ),
+            '01:23:45:67:65:ab:cd:ef': MagicMock(
+                id='01:23:45:67:65:ab:cd:ef',
+                interfaces = {},
+            ),
+            '01:23:45:67:66:ab:cd:ef': MagicMock(
+                id='01:23:45:67:66:ab:cd:ef',
+                interfaces = {},
+            ),
+        }
+        # Initialize Interfaces
+        self.controller.switches['01:23:45:67:89:ab:cd:ef'].interfaces = {
+            0: MagicMock(
+                id = '01:23:45:67:89:ab:cd:ef:0',
+                switch = self.controller.switches['01:23:45:67:89:ab:cd:ef'],
+                link = None,
+            ),
+            1: MagicMock(
+                id = '01:23:45:67:89:ab:cd:ef:1',
+                switch = self.controller.switches['01:23:45:67:89:ab:cd:ef'],
+                link = None,
+            ),
+            2: MagicMock(
+                id = '01:23:45:67:89:ab:cd:ef:2',
+                switch = self.controller.switches['01:23:45:67:89:ab:cd:ef'],
+                link = None,
+            ),
+        }
 
-    def test_start_mw_case_1(self):
-        """Test the method that starts a maintenance."""
+        self.controller.switches['01:23:45:67:65:ab:cd:ef'].interfaces = {
+            0: MagicMock(
+                id = '01:23:45:67:65:ab:cd:ef:0',
+                switch = self.controller.switches['01:23:45:67:65:ab:cd:ef'],
+                link = None,
+            ),
+            1: MagicMock(
+                id = '01:23:45:67:65:ab:cd:ef:1',
+                switch = self.controller.switches['01:23:45:67:65:ab:cd:ef'],
+                link = None,
+            ),
+            2: MagicMock(
+                id = '01:23:45:67:65:ab:cd:ef:2',
+                switch = self.controller.switches['01:23:45:67:65:ab:cd:ef'],
+                link = None,
+            ),
+        }
+
+        self.controller.switches['01:23:45:67:66:ab:cd:ef'].interfaces = {
+            0: MagicMock(
+                id = '01:23:45:67:66:ab:cd:ef:0',
+                switch = self.controller.switches['01:23:45:67:66:ab:cd:ef'],
+                link = None,
+            ),
+            1: MagicMock(
+                id = '01:23:45:67:66:ab:cd:ef:1',
+                switch = self.controller.switches['01:23:45:67:66:ab:cd:ef'],
+                link = None,
+            ),
+            2: MagicMock(
+                id = '01:23:45:67:66:ab:cd:ef:2',
+                switch = self.controller.switches['01:23:45:67:66:ab:cd:ef'],
+                link = None,
+            ),
+        }
+
+        # Initialize Links
+        self.link_1 = MagicMock(
+            id = 'link_1',
+            endpoint_a = self.controller.switches['01:23:45:67:89:ab:cd:ef'].interfaces[0],
+            endpoint_b = self.controller.switches['01:23:45:67:65:ab:cd:ef'].interfaces[0],
+        )
+        self.controller.switches['01:23:45:67:89:ab:cd:ef'].interfaces[0].link = self.link_1
+        self.controller.switches['01:23:45:67:65:ab:cd:ef'].interfaces[0].link = self.link_1
+
+        self.link_2 = MagicMock(
+            id = 'link_2',
+            endpoint_a = self.controller.switches['01:23:45:67:89:ab:cd:ef'].interfaces[1],
+            endpoint_b = self.controller.switches['01:23:45:67:66:ab:cd:ef'].interfaces[0],
+        )
+        self.controller.switches['01:23:45:67:89:ab:cd:ef'].interfaces[1].link = self.link_2
+        self.controller.switches['01:23:45:67:66:ab:cd:ef'].interfaces[0].link = self.link_2
+
+        self.link_3 = MagicMock(
+            id = 'link_3',
+            endpoint_a = self.controller.switches['01:23:45:67:66:ab:cd:ef'].interfaces[1],
+            endpoint_b = self.controller.switches['01:23:45:67:66:ab:cd:ef'].interfaces[2],
+        )
+        self.controller.switches['01:23:45:67:66:ab:cd:ef'].interfaces[1].link = self.link_3
+        self.controller.switches['01:23:45:67:66:ab:cd:ef'].interfaces[2].link = self.link_3
+
+
+        self.controller.napps[('kytos', 'topology')] = MagicMock(
+            links = {
+                'link_1': self.link_1,
+                'link_2': self.link_2,
+                'link_3': self.link_3,
+            }
+        )
+
+    def test_mw_case_1(self):
+        """Test deploying a maintenance window to switches."""
         buffer_put_mock = MagicMock()
         self.controller.buffers.app.put = buffer_put_mock
         maintenance = self.maintenance.copy(
@@ -111,99 +214,238 @@ class TestDeployer(TestCase):
             }
         )
         self.deployer.start_mw(maintenance)
-        buffer_put_mock.assert_called_once()
+        args, kwargs = buffer_put_mock.call_args
+        event = args[0]
+        assert event.name == 'kytos/topology.interruption.start'
+        assert event.content['type'] == 'maintenance'
+        assert sorted(event.content['switches']) == [
+            '01:23:45:67:65:ab:cd:ef',
+            '01:23:45:67:89:ab:cd:ef',
+        ]
+        assert sorted(event.content['interfaces']) == [
+            '01:23:45:67:65:ab:cd:ef:0',
+            '01:23:45:67:65:ab:cd:ef:1',
+            '01:23:45:67:65:ab:cd:ef:2',
+            '01:23:45:67:89:ab:cd:ef:0',
+            '01:23:45:67:89:ab:cd:ef:1',
+            '01:23:45:67:89:ab:cd:ef:2',
+        ]
+        assert sorted(event.content['links']) == [
+            'link_1',
+            'link_2',
+        ]
 
-    def test_start_mw_case_2(self):
-        """Test the method that starts a maintenance."""
+        # Check whats in maintenance
+        # Switches
+        assert not self.deployer.switch_not_in_maintenance(self.controller.switches['01:23:45:67:89:ab:cd:ef'])
+        assert not self.deployer.switch_not_in_maintenance(self.controller.switches['01:23:45:67:65:ab:cd:ef'])
+        assert     self.deployer.switch_not_in_maintenance(self.controller.switches['01:23:45:67:66:ab:cd:ef'])
+        # Interfaces
+        for interface in self.controller.switches['01:23:45:67:89:ab:cd:ef'].interfaces.values():
+            assert not self.deployer.interface_not_in_maintenance(interface)
+        for interface in self.controller.switches['01:23:45:67:65:ab:cd:ef'].interfaces.values():
+            assert not self.deployer.interface_not_in_maintenance(interface)
+        for interface in self.controller.switches['01:23:45:67:66:ab:cd:ef'].interfaces.values():
+            assert     self.deployer.interface_not_in_maintenance(interface)
+        # Links
+        assert not self.deployer.link_not_in_maintenance(self.link_1)
+        assert not self.deployer.link_not_in_maintenance(self.link_2)
+        assert     self.deployer.link_not_in_maintenance(self.link_3)
+
+        self.deployer.end_mw(maintenance)
+        args, kwargs = buffer_put_mock.call_args
+        event = args[0]
+        assert event.name == 'kytos/topology.interruption.end'
+        assert event.content['type'] == 'maintenance'
+        assert sorted(event.content['switches']) == [
+            '01:23:45:67:65:ab:cd:ef',
+            '01:23:45:67:89:ab:cd:ef',
+        ]
+        assert sorted(event.content['interfaces']) == [
+            '01:23:45:67:65:ab:cd:ef:0',
+            '01:23:45:67:65:ab:cd:ef:1',
+            '01:23:45:67:65:ab:cd:ef:2',
+            '01:23:45:67:89:ab:cd:ef:0',
+            '01:23:45:67:89:ab:cd:ef:1',
+            '01:23:45:67:89:ab:cd:ef:2',
+        ]
+        assert sorted(event.content['links']) == [
+            'link_1',
+            'link_2',
+        ]
+        # Check whats in maintenance
+        # Switches
+        assert     self.deployer.switch_not_in_maintenance(self.controller.switches['01:23:45:67:89:ab:cd:ef'])
+        assert     self.deployer.switch_not_in_maintenance(self.controller.switches['01:23:45:67:65:ab:cd:ef'])
+        assert     self.deployer.switch_not_in_maintenance(self.controller.switches['01:23:45:67:66:ab:cd:ef'])
+        # Interfaces
+        for interface in self.controller.switches['01:23:45:67:89:ab:cd:ef'].interfaces.values():
+            assert     self.deployer.interface_not_in_maintenance(interface)
+        for interface in self.controller.switches['01:23:45:67:65:ab:cd:ef'].interfaces.values():
+            assert     self.deployer.interface_not_in_maintenance(interface)
+        for interface in self.controller.switches['01:23:45:67:66:ab:cd:ef'].interfaces.values():
+            assert     self.deployer.interface_not_in_maintenance(interface)
+        # Links
+        assert     self.deployer.link_not_in_maintenance(self.link_1)
+        assert     self.deployer.link_not_in_maintenance(self.link_2)
+        assert     self.deployer.link_not_in_maintenance(self.link_3)
+
+    def test_mw_case_2(self):
+        """Test deploying a maintenance window to interfaces."""
         buffer_put_mock = MagicMock()
         self.controller.buffers.app.put = buffer_put_mock
-        interface_id = "interface_1"
+        
         maintenance = self.maintenance.copy(
             update = {
-                'switches': [
-                    '01:23:45:67:89:ab:cd:ef',
-                    '01:23:45:67:65:ab:cd:ef'
+                'switches': [],
+                'interfaces': [
+                    '01:23:45:67:65:ab:cd:ef:0',
+                    '01:23:45:67:89:ab:cd:ef:0',
+                    '01:23:45:67:89:ab:cd:ef:1',
                 ],
-                'interfaces': [interface_id],
                 'links': [],
             }
         )
         self.deployer.start_mw(maintenance)
-        self.assertEqual(buffer_put_mock.call_count, 1)
+        args, kwargs = buffer_put_mock.call_args
+        event = args[0]
+        assert event.name == 'kytos/topology.interruption.start'
+        assert event.content['type'] == 'maintenance'
+        assert sorted(event.content['switches']) == []
+        assert sorted(event.content['interfaces']) == [
+            '01:23:45:67:65:ab:cd:ef:0',
+            '01:23:45:67:89:ab:cd:ef:0',
+            '01:23:45:67:89:ab:cd:ef:1',
+        ]
+        assert sorted(event.content['links']) == [
+            'link_1',
+            'link_2',
+        ]
 
-    def test_start_mw_case_3(self):
-        """Test the method that starts a maintenance."""
+        # Check whats in maintenance
+        # Switches
+        assert     self.deployer.switch_not_in_maintenance(self.controller.switches['01:23:45:67:89:ab:cd:ef'])
+        assert     self.deployer.switch_not_in_maintenance(self.controller.switches['01:23:45:67:65:ab:cd:ef'])
+        assert     self.deployer.switch_not_in_maintenance(self.controller.switches['01:23:45:67:66:ab:cd:ef'])
+        # Interfaces
+
+        assert not self.deployer.interface_not_in_maintenance(self.controller.switches['01:23:45:67:89:ab:cd:ef'].interfaces[0])
+        assert not self.deployer.interface_not_in_maintenance(self.controller.switches['01:23:45:67:89:ab:cd:ef'].interfaces[1])
+        assert     self.deployer.interface_not_in_maintenance(self.controller.switches['01:23:45:67:89:ab:cd:ef'].interfaces[2])
+
+        assert not self.deployer.interface_not_in_maintenance(self.controller.switches['01:23:45:67:65:ab:cd:ef'].interfaces[0])
+        assert     self.deployer.interface_not_in_maintenance(self.controller.switches['01:23:45:67:65:ab:cd:ef'].interfaces[1])
+        assert     self.deployer.interface_not_in_maintenance(self.controller.switches['01:23:45:67:65:ab:cd:ef'].interfaces[2])
+
+        for interface in self.controller.switches['01:23:45:67:66:ab:cd:ef'].interfaces.values():
+            assert     self.deployer.interface_not_in_maintenance(interface)
+
+        # Links
+        assert not self.deployer.link_not_in_maintenance(self.link_1)
+        assert not self.deployer.link_not_in_maintenance(self.link_2)
+        assert     self.deployer.link_not_in_maintenance(self.link_3)
+
+        self.deployer.end_mw(maintenance)
+        args, kwargs = buffer_put_mock.call_args
+        event = args[0]
+        assert event.name == 'kytos/topology.interruption.end'
+        assert event.content['type'] == 'maintenance'
+        assert sorted(event.content['switches']) == []
+        assert sorted(event.content['switches']) == []
+        assert sorted(event.content['interfaces']) == [
+            '01:23:45:67:65:ab:cd:ef:0',
+            '01:23:45:67:89:ab:cd:ef:0',
+            '01:23:45:67:89:ab:cd:ef:1',
+        ]
+        assert sorted(event.content['links']) == [
+            'link_1',
+            'link_2',
+        ]
+        # Check whats in maintenance
+        # Switches
+        assert     self.deployer.switch_not_in_maintenance(self.controller.switches['01:23:45:67:89:ab:cd:ef'])
+        assert     self.deployer.switch_not_in_maintenance(self.controller.switches['01:23:45:67:65:ab:cd:ef'])
+        assert     self.deployer.switch_not_in_maintenance(self.controller.switches['01:23:45:67:66:ab:cd:ef'])
+        # Interfaces
+        for interface in self.controller.switches['01:23:45:67:89:ab:cd:ef'].interfaces.values():
+            assert     self.deployer.interface_not_in_maintenance(interface)
+        for interface in self.controller.switches['01:23:45:67:65:ab:cd:ef'].interfaces.values():
+            assert     self.deployer.interface_not_in_maintenance(interface)
+        for interface in self.controller.switches['01:23:45:67:66:ab:cd:ef'].interfaces.values():
+            assert     self.deployer.interface_not_in_maintenance(interface)
+        # Links
+        assert     self.deployer.link_not_in_maintenance(self.link_1)
+        assert     self.deployer.link_not_in_maintenance(self.link_2)
+        assert     self.deployer.link_not_in_maintenance(self.link_3)
+
+    def test_mw_case_3(self):
+        """Test deploying a maintenance window to links."""
         buffer_put_mock = MagicMock()
         self.controller.buffers.app.put = buffer_put_mock
-        interface_id = "interface_1"
-        link1 = "link_1"
-        link2 = "link_2"
         maintenance = self.maintenance.copy(
             update = {
-                'switches': [
-                ],
-                'interfaces': [interface_id],
-                'links': [link1, link2],
-            }
-        )
-        self.deployer.start_mw(maintenance)
-        self.assertEqual(buffer_put_mock.call_count, 1)
-
-    def test_end_mw_case_1(self):
-        """Test the method that ends a maintenance."""
-        buffer_put_mock = MagicMock()
-        self.controller.buffers.app.put = buffer_put_mock
-        maintenance = self.maintenance.copy(
-            update = {
-                'switches': [
-                    '01:23:45:67:89:ab:cd:ef',
-                    '01:23:45:67:65:ab:cd:ef'
-                ],
+                'switches': [],
                 'interfaces': [],
-                'links': [],
-                'status': Status.RUNNING,
+                'links': ['link_1', 'link_2'],
             }
         )
-        self.deployer.end_mw(maintenance)
-        buffer_put_mock.assert_called_once()
+        self.deployer.start_mw(maintenance)
+        args, kwargs = buffer_put_mock.call_args
+        event = args[0]
+        assert event.name == 'kytos/topology.interruption.start'
+        assert event.content['type'] == 'maintenance'
+        assert sorted(event.content['switches']) == []
+        assert sorted(event.content['interfaces']) == []
+        assert sorted(event.content['links']) == [
+            'link_1',
+            'link_2',
+        ]
 
-    def test_end_mw_case_2(self):
-        """Test the method that ends a maintenance."""
-        buffer_put_mock = MagicMock()
-        self.controller.buffers.app.put = buffer_put_mock
-        interface_id = "interface_1"
-        maintenance = self.maintenance.copy(
-            update = {
-                'switches': [
-                    '01:23:45:67:89:ab:cd:ef',
-                    '01:23:45:67:65:ab:cd:ef'
-                ],
-                'interfaces': [interface_id],
-                'links': [],
-                'status': Status.RUNNING,
-            }
-        )
-        self.deployer.end_mw(maintenance)
-        self.assertEqual(buffer_put_mock.call_count, 1)
+        # Check whats in maintenance
+        # Switches
+        assert     self.deployer.switch_not_in_maintenance(self.controller.switches['01:23:45:67:89:ab:cd:ef'])
+        assert     self.deployer.switch_not_in_maintenance(self.controller.switches['01:23:45:67:65:ab:cd:ef'])
+        assert     self.deployer.switch_not_in_maintenance(self.controller.switches['01:23:45:67:66:ab:cd:ef'])
+        # Interfaces
+        for interface in self.controller.switches['01:23:45:67:89:ab:cd:ef'].interfaces.values():
+            assert     self.deployer.interface_not_in_maintenance(interface)
+        for interface in self.controller.switches['01:23:45:67:65:ab:cd:ef'].interfaces.values():
+            assert     self.deployer.interface_not_in_maintenance(interface)
+        for interface in self.controller.switches['01:23:45:67:66:ab:cd:ef'].interfaces.values():
+            assert     self.deployer.interface_not_in_maintenance(interface)
+        # Links
+        assert not self.deployer.link_not_in_maintenance(self.link_1)
+        assert not self.deployer.link_not_in_maintenance(self.link_2)
+        assert     self.deployer.link_not_in_maintenance(self.link_3)
 
-    def test_end_mw_case_3(self):
-        """Test the method that ends a maintenance."""
-        buffer_put_mock = MagicMock()
-        self.controller.buffers.app.put = buffer_put_mock
-        interface_id = "interface_1"
-        link1 = "link_1"
-        link2 = "link_2"
-        maintenance = self.maintenance.copy(
-            update = {
-                'switches': [
-                ],
-                'interfaces': [interface_id],
-                'links': [link1, link2],
-                'status': Status.RUNNING,
-            }
-        )
         self.deployer.end_mw(maintenance)
-        self.assertEqual(buffer_put_mock.call_count, 1)
+        args, kwargs = buffer_put_mock.call_args
+        event = args[0]
+        assert event.name == 'kytos/topology.interruption.end'
+        assert event.content['type'] == 'maintenance'
+        assert sorted(event.content['switches']) == []
+        assert sorted(event.content['interfaces']) == []
+        assert sorted(event.content['links']) == [
+            'link_1',
+            'link_2',
+        ]
+        # Check whats in maintenance
+        # Switches
+        assert     self.deployer.switch_not_in_maintenance(self.controller.switches['01:23:45:67:89:ab:cd:ef'])
+        assert     self.deployer.switch_not_in_maintenance(self.controller.switches['01:23:45:67:65:ab:cd:ef'])
+        assert     self.deployer.switch_not_in_maintenance(self.controller.switches['01:23:45:67:66:ab:cd:ef'])
+        # Interfaces
+        for interface in self.controller.switches['01:23:45:67:89:ab:cd:ef'].interfaces.values():
+            assert     self.deployer.interface_not_in_maintenance(interface)
+        for interface in self.controller.switches['01:23:45:67:65:ab:cd:ef'].interfaces.values():
+            assert     self.deployer.interface_not_in_maintenance(interface)
+        for interface in self.controller.switches['01:23:45:67:66:ab:cd:ef'].interfaces.values():
+            assert     self.deployer.interface_not_in_maintenance(interface)
+        # Links
+        assert     self.deployer.link_not_in_maintenance(self.link_1)
+        assert     self.deployer.link_not_in_maintenance(self.link_2)
+        assert     self.deployer.link_not_in_maintenance(self.link_3)
+
 
 class TestScheduler(TestCase):
     """Test of the Scheduler Class"""
