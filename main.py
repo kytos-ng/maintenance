c89e267a0a04eddc28443df36f77e5e74cdb6a99
HeriLFIU <hluna006@fiu.edu>
2024-02-28 13:05:38 -0500

    Reformatted code with Black

--- main.py
+++ main.py
@@ -14,8 +14,14 @@ from pydantic import ValidationError
 from pymongo.errors import DuplicateKeyError
 
 from kytos.core import KytosNApp, rest
-from kytos.core.rest_api import (HTTPException, JSONResponse, Request,
-                                 Response, error_msg, get_json_or_400)
+from kytos.core.rest_api import (
+    HTTPException,
+    JSONResponse,
+    Request,
+    Response,
+    error_msg,
+    get_json_or_400,
+)
 
 
 class Main(KytosNApp):
@@ -52,7 +58,7 @@ class Main(KytosNApp):
         """
         self.scheduler.shutdown()
 
-    @rest('/v1', methods=['GET'])
+    @rest("/v1", methods=["GET"])
     def get_all_mw(self, _request: Request) -> Response:
         """Return all maintenance windows."""
         maintenances = self.scheduler.list_maintenances()
@@ -62,7 +68,7 @@ class Main(KytosNApp):
             media_type="application/json",
         )
 
-    @rest('/v1/{mw_id}', methods=['GET'])
+    @rest("/v1/{mw_id}", methods=["GET"])
     def get_mw(self, request: Request) -> Response:
         """Return one maintenance window."""
         mw_id: MaintenanceID = request.path_params["mw_id"]
@@ -73,18 +79,18 @@ class Main(KytosNApp):
                 status_code=200,
                 media_type="application/json",
             )
-        raise HTTPException(404, f'Maintenance with id {mw_id} not found')
+        raise HTTPException(404, f"Maintenance with id {mw_id} not found")
 
-    @rest('/v1', methods=['POST'])
+    @rest("/v1", methods=["POST"])
     def create_mw(self, request: Response) -> JSONResponse:
         """Create a new maintenance window."""
         data = get_json_or_400(request, self.controller.loop)
         if not isinstance(data, dict) or not data:
             raise HTTPException(400, detail=f"Invalid json body value: {data}")
 
-        if 'status' in data:
+        if "status" in data:
             raise HTTPException(
-                400, detail='Setting a maintenance status is not allowed'
+                400, detail="Setting a maintenance status is not allowed"
             )
         # if 'id' in data:
         #     raise HTTPException(
@@ -92,8 +98,8 @@ class Main(KytosNApp):
         #     )
         try:
             maintenance = MW.parse_obj(data)
-            force = data.get('force', False)
-            ignore_no_exists = data.get('ignore_no_exists')
+            force = data.get("force", False)
+            ignore_no_exists = data.get("ignore_no_exists")
             if not ignore_no_exists:
                 self.validate_item_existence(maintenance)
             self.scheduler.add(maintenance, force=force)
@@ -102,16 +108,15 @@ class Main(KytosNApp):
             raise HTTPException(400, detail=msg) from err
         except DuplicateKeyError as err:
             raise HTTPException(
-                409,
-                detail=f'Window with id: {maintenance.id} already exists'
+                409, detail=f"Window with id: {maintenance.id} already exists"
             ) from err
         except OverlapError as err:
-            raise HTTPException(400, detail=f'{err}') from err
+            raise HTTPException(400, detail=f"{err}") from err
         except ValueError as err:
-            raise HTTPException(400, detail=f'{err}') from err
-        return JSONResponse({'mw_id': maintenance.id}, status_code=201)
+            raise HTTPException(400, detail=f"{err}") from err
+        return JSONResponse({"mw_id": maintenance.id}, status_code=201)
 
-    @rest('/v1/{mw_id}', methods=['PATCH'])
+    @rest("/v1/{mw_id}", methods=["PATCH"])
     def update_mw(self, request: Request) -> JSONResponse:
         """Update a maintenance window."""
         data = get_json_or_400(request, self.controller.loop)
@@ -121,16 +126,14 @@ class Main(KytosNApp):
         mw_id: MaintenanceID = request.path_params["mw_id"]
         old_maintenance = self.scheduler.get_maintenance(mw_id)
         if old_maintenance is None:
-            raise HTTPException(
-                404, detail=f'Maintenance with id {mw_id} not found'
-            )
+            raise HTTPException(404, detail=f"Maintenance with id {mw_id} not found")
         if old_maintenance.status == Status.RUNNING:
             raise HTTPException(
-                400, detail='Updating a running maintenance is not allowed'
+                400, detail="Updating a running maintenance is not allowed"
             )
-        if 'status' in data:
+        if "status" in data:
             raise HTTPException(
-                400, detail='Updating a maintenance status is not allowed'
+                400, detail="Updating a maintenance status is not allowed"
             )
         try:
             new_maintenance = MW.parse_obj({**old_maintenance.dict(), **data})
@@ -138,49 +141,45 @@ class Main(KytosNApp):
             msg = error_msg(err.errors())
             raise HTTPException(400, detail=msg) from err
         if new_maintenance.id != old_maintenance.id:
-            raise HTTPException(400, detail='Updated id must match old id')
+            raise HTTPException(400, detail="Updated id must match old id")
         self.scheduler.update(new_maintenance)
-        return JSONResponse({'response': f'Maintenance {mw_id} updated'})
+        return JSONResponse({"response": f"Maintenance {mw_id} updated"})
 
-    @rest('/v1/{mw_id}', methods=['DELETE'])
+    @rest("/v1/{mw_id}", methods=["DELETE"])
     def remove_mw(self, request: Request) -> JSONResponse:
         """Delete a maintenance window."""
         mw_id: MaintenanceID = request.path_params["mw_id"]
         maintenance = self.scheduler.get_maintenance(mw_id)
         if maintenance is None:
-            raise HTTPException(
-                404, detail=f'Maintenance with id {mw_id} not found'
-            )
+            raise HTTPException(404, detail=f"Maintenance with id {mw_id} not found")
         if maintenance.status == Status.RUNNING:
             raise HTTPException(
-                400, detail='Deleting a running maintenance is not allowed'
+                400, detail="Deleting a running maintenance is not allowed"
             )
         self.scheduler.remove(mw_id)
-        return JSONResponse({'response': f'Maintenance with id {mw_id} '
-                                         f'successfully removed'})
+        return JSONResponse(
+            {"response": f"Maintenance with id {mw_id} " f"successfully removed"}
+        )
 
-    @rest('/v1/{mw_id}/end', methods=['PATCH'])
+    @rest("/v1/{mw_id}/end", methods=["PATCH"])
     def end_mw(self, request: Request) -> JSONResponse:
         """Finish a maintenance window right now."""
         mw_id: MaintenanceID = request.path_params["mw_id"]
         maintenance = self.scheduler.get_maintenance(mw_id)
         if maintenance is None:
-            raise HTTPException(
-                404, detail=f'Maintenance with id {mw_id} not found'
-            )
+            raise HTTPException(404, detail=f"Maintenance with id {mw_id} not found")
         if maintenance.status == Status.PENDING:
             raise HTTPException(
-                400, detail=f'Maintenance window {mw_id} has not yet started'
+                400, detail=f"Maintenance window {mw_id} has not yet started"
             )
         if maintenance.status == Status.FINISHED:
             raise HTTPException(
-                400, detail=f'Maintenance window {mw_id} has already finished'
+                400, detail=f"Maintenance window {mw_id} has already finished"
             )
         self.scheduler.end_maintenance_early(mw_id)
-        return JSONResponse({'response': f'Maintenance window {mw_id} '
-                                         f'finished'})
+        return JSONResponse({"response": f"Maintenance window {mw_id} " f"finished"})
 
-    @rest('/v1/{mw_id}/extend', methods=['PATCH'])
+    @rest("/v1/{mw_id}/extend", methods=["PATCH"])
     def extend_mw(self, request: Request) -> JSONResponse:
         """Extend a running maintenance window."""
         mw_id: MaintenanceID = request.path_params["mw_id"]
@@ -190,76 +189,57 @@ class Main(KytosNApp):
 
         maintenance = self.scheduler.get_maintenance(mw_id)
         if maintenance is None:
-            raise HTTPException(
-                404, detail=f'Maintenance with id {mw_id} not found'
-            )
-        if 'minutes' not in data:
-            raise HTTPException(
-                400,
-                detail='Minutes of extension must be sent'
-            )
+            raise HTTPException(404, detail=f"Maintenance with id {mw_id} not found")
+        if "minutes" not in data:
+            raise HTTPException(400, detail="Minutes of extension must be sent")
         if maintenance.status == Status.PENDING:
             raise HTTPException(
-                400,
-                detail=f'Maintenance window {mw_id} has not yet started'
+                400, detail=f"Maintenance window {mw_id} has not yet started"
             )
         if maintenance.status == Status.FINISHED:
             raise HTTPException(
-                400,
-                detail=f'Maintenance window {mw_id} has already finished'
+                400, detail=f"Maintenance window {mw_id} has already finished"
             )
         try:
-            maintenance_end = maintenance.end + \
-                timedelta(minutes=data['minutes'])
-            new_maintenance = maintenance.copy(
-                update={'end': maintenance_end}
-            )
+            maintenance_end = maintenance.end + timedelta(minutes=data["minutes"])
+            new_maintenance = maintenance.copy(update={"end": maintenance_end})
         except TypeError as exc:
             raise HTTPException(
-                400,
-                detail='Minutes of extension must be integer') from exc
+                400, detail="Minutes of extension must be integer"
+            ) from exc
 
         self.scheduler.update(new_maintenance)
-        return JSONResponse({'response': f'Maintenance {mw_id} extended'})
+        return JSONResponse({"response": f"Maintenance {mw_id} extended"})
 
     def validate_item_existence(self, window: MW):
         """Validate that all items in a maintenance window exist."""
         non_existant_switches = list(
             filter(
-                lambda switch_id:
-                    self.controller.switches.get(switch_id)
-                    is None,
-                window.switches
+                lambda switch_id: self.controller.switches.get(switch_id) is None,
+                window.switches,
             )
         )
         non_existant_interfaces = list(
             filter(
-                lambda interface_id:
-                    self.controller.get_interface_by_id(interface_id)
-                    is None,
-                window.interfaces
+                lambda interface_id: self.controller.get_interface_by_id(interface_id)
+                is None,
+                window.interfaces,
             )
         )
         non_existant_links = list(
             filter(
-                lambda link_id:
-                    self.controller.napps[('kytos', 'topology')]
-                    .links.get(link_id)
-                    is None,
-                window.interfaces
+                lambda link_id: self.controller.napps[("kytos", "topology")].links.get(
+                    link_id
+                )
+                is None,
+                window.interfaces,
             )
         )
 
-        if (
-            non_existant_switches
-            or non_existant_interfaces
-            or non_existant_links
-        ):
+        if non_existant_switches or non_existant_interfaces or non_existant_links:
             items = {
-                'switches': non_existant_switches,
-                'interfaces': non_existant_interfaces,
-                'links': non_existant_links,
+                "switches": non_existant_switches,
+                "interfaces": non_existant_interfaces,
+                "links": non_existant_links,
             }
-            raise HTTPException(
-                400,
-                f"Window contains non-existant items: {items}")
+            raise HTTPException(400, f"Window contains non-existant items: {items}")
